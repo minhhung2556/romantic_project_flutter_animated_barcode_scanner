8ad19001eb0521f89c590122ec15f9ea2a30d7a3
Hung Luong Do Minh <minhhung2556@gmail.com>
2024-11-14 16:58:25 +0700

    fix: update demo

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,36 +1,87 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_animated_qr_scanner/flutter_animated_qr_scanner.dart';
+
+const kPreferredOrientations = [
+  DeviceOrientation.portraitUp,
+  DeviceOrientation.landscapeLeft,
+];
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(kPreferredOrientations);
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  @override
-  MyAppState createState() => MyAppState();
-}
-
-class MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Example',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Home(),
+      theme: ThemeData.from(colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue)),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomeScreen(),
+        '/barcodeScanner': (context) => BarcodeScannerScreen(),
+      },
     );
   }
 }
 
-class Home extends StatelessWidget {
-  const Home({super.key});
+class BarcodeScannerScreen extends StatelessWidget {
+  const BarcodeScannerScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BarcodeScannerPreview();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Barcode Scanner'),
+      ),
+      body: Column(
+        children: [
+          SizedBox.square(
+            dimension: MediaQuery.of(context).size.shortestSide,
+            child: BarcodeScannerPreview(
+              originalPreferredOrientations: kPreferredOrientations,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Example'),
+      ),
+      body: Column(
+        children: [
+          FilledButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/barcodeScanner');
+            },
+            child: Text('Open barcode scanner'),
+          ),
+          FilledButton(
+            onPressed: () {
+              if (MediaQuery.of(context).orientation == Orientation.portrait) {
+                SystemChrome.setPreferredOrientations(kPreferredOrientations.reversed.toList(growable: false));
+              } else {
+                SystemChrome.setPreferredOrientations(kPreferredOrientations);
+              }
+            },
+            child: const Text('Toggle Orientation'),
+          ),
+        ],
+      ),
+    );
   }
 }
